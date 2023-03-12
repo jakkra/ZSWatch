e21e38ccfabadad33835e0249d57c24b5e4a919b
Jakob Krantz <mail@jakobkrantz.se>
2023-03-13 00:29:20 +0100

    Add BMI270 port and drivers from Bosch as submodule.
    Will use Bosch drivers as they are way more powerful compared
    to what is right now implemented in Zephyr.

--- app/src/accelerometer.c
+++ app/src/accelerometer.c
@@ -1,4 +1,6 @@
 #include <accelerometer.h>
+#include <bmi270_port.h>
+#include <bmi270.h>
 #include <zephyr/logging/log.h>
 #include <events/accel_event.h>
 
@@ -9,51 +11,72 @@ static void data_ready_xyz(const struct device *dev,
 static int configure_double_tap_detection(void);
 static int configure_pedometer(void);
 static int configure_tilt_detection(void);
+static int8_t set_accel_gyro_config(struct bmi2_dev *bmi2_dev);
+
 static void send_accel_event(accelerometer_evt_t *data);
 
 static const struct device *sensor;
 static accel_event_cb accel_evt_cb;
 
+/*! Earth's gravity in m/s^2 */
+#define GRAVITY_EARTH  (9.80665f)
+
+/*! Macros to select the sensors                   */
+#define ACCEL          UINT8_C(0x00)
+#define GYRO           UINT8_C(0x01)
+
 int accelerometer_init(accel_event_cb cb)
 {
-    struct sensor_value odr;
-    struct sensor_trigger trig;
-    sensor = DEVICE_DT_GET_ONE(bosch_bmi270);
-    if (!device_is_ready(sensor)) {
-        LOG_ERR("Error: Device \"%s\" is not ready; "
-                "check the driver initialization logs for errors.",
-                sensor->name);
-        return -ENODEV;
-    }
+    /* Status of api are returned to this variable. */
+    int8_t rslt;
 
-    // 400Hz needed for double tap detection
-    odr.val1 = 400;
-    odr.val2 = 0;
-    int err = sensor_attr_set(sensor, SENSOR_CHAN_ACCEL_XYZ, SENSOR_ATTR_SAMPLING_FREQUENCY, &odr);
-    if (!err) {
-        trig.type = SENSOR_TRIG_DATA_READY;
-        trig.chan = SENSOR_CHAN_ALL;
-        err = sensor_trigger_set(sensor, &trig, data_ready_xyz);
-        if (err == 0) {
-            accel_evt_cb = cb;
-            err = configure_double_tap_detection();
-            if (err < 0) {
-                LOG_ERR("configure_double_tap_detection failed: %d", err);
-            }
-            err = configure_pedometer();
-            if (err < 0) {
-                LOG_ERR("configure_pedometer failed: %d", err);
-            }
-            err = configure_tilt_detection();
-            if (err < 0) {
-                LOG_ERR("configure_tilt_detection failed: %d", err);
-            }
+    /* Variable to define limit to print accel data. */
+    uint8_t limit = 10;
+
+    /* Assign accel and gyro sensor to variable. */
+    uint8_t sensor_list[2] = { BMI2_ACCEL, BMI2_GYRO };
+
+    /* Sensor initialization configuration. */
+    struct bmi2_dev bmi2_dev;
+
+    /* Create an instance of sensor data structure. */
+    struct bmi2_sens_data sensor_data = { { 0 } };
+
+    /* Initialize the interrupt status of accel and gyro. */
+    uint16_t int_status = 0;
+
+    uint8_t indx = 1;
+
+    float x = 0, y = 0, z = 0;
+
+    /* Interface reference is given as a parameter
+     * For I2C : BMI2_I2C_INTF
+     * For SPI : BMI2_SPI_INTF
+     */
+    rslt = bmi2_interface_init(&bmi2_dev, BMI2_I2C_INTF);
+    bmi2_error_codes_print_result(rslt);
+
+    /* Initialize bmi270. */
+    rslt = bmi270_init(&bmi2_dev);
+    bmi2_error_codes_print_result(rslt);
+
+    if (rslt == BMI2_OK)
+    {
+        /* Accel and gyro configuration settings. */
+        rslt = set_accel_gyro_config(&bmi2_dev);
+        bmi2_error_codes_print_result(rslt);
+
+        if (rslt == BMI2_OK)
+        {
+            /* NOTE:
+             * Accel and Gyro enable must be done after setting configurations
+             */
+            rslt = bmi270_sensor_enable(sensor_list, 2, &bmi2_dev);
+            bmi2_error_codes_print_result(rslt);
         }
-    } else {
-        LOG_ERR("sensor_attr_set fail: %d", err);
     }
 
-    return err;
+    return 0;
 }
 
 int accelerometer_fetch_xyz(int16_t *x, int16_t *y, int16_t *z)
@@ -158,7 +181,6 @@ static void data_ready_xyz(const struct device *dev, const struct sensor_trigger
     }
 
     */
-
 }
 
 static int configure_double_tap_detection(void)
@@ -182,4 +204,88 @@ static void send_accel_event(accelerometer_evt_t *data)
 
     memcpy(&event->data, data, sizeof(accelerometer_evt_t));
     APP_EVENT_SUBMIT(event);
+}
+
+
+/*!
+ * @brief This internal API is used to set configurations for accel and gyro.
+ */
+static int8_t set_accel_gyro_config(struct bmi2_dev *bmi2_dev)
+{
+    /* Status of api are returned to this variable. */
+    int8_t rslt;
+
+    /* Structure to define accelerometer and gyro configuration. */
+    struct bmi2_sens_config config[2];
+
+    /* Configure the type of feature. */
+    config[ACCEL].type = BMI2_ACCEL;
+    config[GYRO].type = BMI2_GYRO;
+
+    /* Get default configurations for the type of feature selected. */
+    rslt = bmi270_get_sensor_config(config, 2, bmi2_dev);
+    bmi2_error_codes_print_result(rslt);
+
+    /* Map data ready interrupt to interrupt pin. */
+    rslt = bmi2_map_data_int(BMI2_DRDY_INT, BMI2_INT1, bmi2_dev);
+    bmi2_error_codes_print_result(rslt);
+
+    if (rslt == BMI2_OK)
+    {
+        /* NOTE: The user can change the following configuration parameters according to their requirement. */
+        /* Set Output Data Rate */
+        config[ACCEL].cfg.acc.odr = BMI2_ACC_ODR_200HZ;
+
+        /* Gravity range of the sensor (+/- 2G, 4G, 8G, 16G). */
+        config[ACCEL].cfg.acc.range = BMI2_ACC_RANGE_2G;
+
+        /* The bandwidth parameter is used to configure the number of sensor samples that are averaged
+         * if it is set to 2, then 2^(bandwidth parameter) samples
+         * are averaged, resulting in 4 averaged samples.
+         * Note1 : For more information, refer the datasheet.
+         * Note2 : A higher number of averaged samples will result in a lower noise level of the signal, but
+         * this has an adverse effect on the power consumed.
+         */
+        config[ACCEL].cfg.acc.bwp = BMI2_ACC_NORMAL_AVG4;
+
+        /* Enable the filter performance mode where averaging of samples
+         * will be done based on above set bandwidth and ODR.
+         * There are two modes
+         *  0 -> Ultra low power mode
+         *  1 -> High performance mode(Default)
+         * For more info refer datasheet.
+         */
+        config[ACCEL].cfg.acc.filter_perf = BMI2_PERF_OPT_MODE;
+
+        /* The user can change the following configuration parameters according to their requirement. */
+        /* Set Output Data Rate */
+        config[GYRO].cfg.gyr.odr = BMI2_GYR_ODR_200HZ;
+
+        /* Gyroscope Angular Rate Measurement Range.By default the range is 2000dps. */
+        config[GYRO].cfg.gyr.range = BMI2_GYR_RANGE_2000;
+
+        /* Gyroscope bandwidth parameters. By default the gyro bandwidth is in normal mode. */
+        config[GYRO].cfg.gyr.bwp = BMI2_GYR_NORMAL_MODE;
+
+        /* Enable/Disable the noise performance mode for precision yaw rate sensing
+         * There are two modes
+         *  0 -> Ultra low power mode(Default)
+         *  1 -> High performance mode
+         */
+        config[GYRO].cfg.gyr.noise_perf = BMI2_POWER_OPT_MODE;
+
+        /* Enable/Disable the filter performance mode where averaging of samples
+         * will be done based on above set bandwidth and ODR.
+         * There are two modes
+         *  0 -> Ultra low power mode
+         *  1 -> High performance mode(Default)
+         */
+        config[GYRO].cfg.gyr.filter_perf = BMI2_PERF_OPT_MODE;
+
+        /* Set the accel and gyro configurations. */
+        rslt = bmi270_set_sensor_config(config, 2, bmi2_dev);
+        bmi2_error_codes_print_result(rslt);
+    }
+
+    return rslt;
 }
---
+++ app/src/ext_drivers/bmi270/bmi270_port.c
@@ -0,0 +1,270 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "bmi270_port.h"
+#include "bmi2_defs.h"
+#include <zephyr/device.h>
+#include <zephyr/drivers/i2c.h>
+
+#define BMI2XY_SHUTTLE_ID  UINT16_C(0x1B8)
+
+/*! Macro that defines read write length */
+#define READ_WRITE_LEN     UINT8_C(46)
+
+static uint8_t dev_addr;
+
+struct config {
+	const struct device *i2c_master;
+};
+
+static const struct config config = {
+	.i2c_master = DEVICE_DT_GET(DT_BUS(DT_NODELABEL(bmi270))),
+};
+
+
+BMI2_INTF_RETURN_TYPE bmi2_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr)
+{
+    uint8_t dev_addr = *(uint8_t*)intf_ptr;
+
+    return i2c_burst_read(config.i2c_master, dev_addr, (uint16_t)reg_addr, reg_data, len);
+}
+
+BMI2_INTF_RETURN_TYPE bmi2_i2c_write(uint8_t reg_addr, const uint8_t *reg_data, uint32_t len, void *intf_ptr)
+{
+    uint8_t dev_addr = *(uint8_t*)intf_ptr;
+
+    return i2c_burst_write(config.i2c_master, dev_addr, reg_addr, reg_data, len);
+}
+
+void bmi2_delay_us(uint32_t period, void *intf_ptr)
+{
+    k_usleep(period);
+}
+
+int8_t bmi2_interface_init(struct bmi2_dev *bmi, uint8_t intf)
+{
+    int8_t rslt = BMI2_OK;
+
+    if (bmi != NULL)
+    {
+        printk("I2C Interface \n");
+
+        /* To initialize the user I2C function */
+        dev_addr = BMI2_I2C_PRIM_ADDR;
+        bmi->intf = BMI2_I2C_INTF;
+        bmi->read = bmi2_i2c_read;
+        bmi->write = bmi2_i2c_write;
+
+        /* Assign device address to interface pointer */
+        bmi->intf_ptr = &dev_addr;
+
+        /* Configure delay in microseconds */
+        bmi->delay_us = bmi2_delay_us;
+
+        /* Configure max read/write length (in bytes) ( Supported length depends on target machine) */
+        bmi->read_write_len = READ_WRITE_LEN;
+
+        /* Assign to NULL to load the default config file. */
+        bmi->config_file_ptr = NULL;
+    }
+    else
+    {
+        rslt = BMI2_E_NULL_PTR;
+    }
+
+    return rslt;
+
+}
+
+void bmi2_error_codes_print_result(int8_t rslt)
+{
+    switch (rslt)
+    {
+        case BMI2_OK:
+
+            /* Do nothing */
+            break;
+
+        case BMI2_W_FIFO_EMPTY:
+            printk("Warning [%d] : FIFO empty\r\n", rslt);
+            break;
+        case BMI2_W_PARTIAL_READ:
+            printk("Warning [%d] : FIFO partial read\r\n", rslt);
+            break;
+        case BMI2_E_NULL_PTR:
+            printk(
+                "Error [%d] : Null pointer error. It occurs when the user tries to assign value (not address) to a pointer," " which has been initialized to NULL.\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_COM_FAIL:
+            printk(
+                "Error [%d] : Communication failure error. It occurs due to read/write operation failure and also due " "to power failure during communication\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_DEV_NOT_FOUND:
+            printk("Error [%d] : Device not found error. It occurs when the device chip id is incorrectly read\r\n",
+                   rslt);
+            break;
+
+        case BMI2_E_INVALID_SENSOR:
+            printk(
+                "Error [%d] : Invalid sensor error. It occurs when there is a mismatch in the requested feature with the " "available one\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_SELF_TEST_FAIL:
+            printk(
+                "Error [%d] : Self-test failed error. It occurs when the validation of accel self-test data is " "not satisfied\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_INVALID_INT_PIN:
+            printk(
+                "Error [%d] : Invalid interrupt pin error. It occurs when the user tries to configure interrupt pins " "apart from INT1 and INT2\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_OUT_OF_RANGE:
+            printk(
+                "Error [%d] : Out of range error. It occurs when the data exceeds from filtered or unfiltered data from " "fifo and also when the range exceeds the maximum range for accel and gyro while performing FOC\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_ACC_INVALID_CFG:
+            printk(
+                "Error [%d] : Invalid Accel configuration error. It occurs when there is an error in accel configuration" " register which could be one among range, BW or filter performance in reg address 0x40\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_GYRO_INVALID_CFG:
+            printk(
+                "Error [%d] : Invalid Gyro configuration error. It occurs when there is a error in gyro configuration" "register which could be one among range, BW or filter performance in reg address 0x42\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_ACC_GYR_INVALID_CFG:
+            printk(
+                "Error [%d] : Invalid Accel-Gyro configuration error. It occurs when there is a error in accel and gyro" " configuration registers which could be one among range, BW or filter performance in reg address 0x40 " "and 0x42\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_CONFIG_LOAD:
+            printk(
+                "Error [%d] : Configuration load error. It occurs when failure observed while loading the configuration " "into the sensor\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_INVALID_PAGE:
+            printk(
+                "Error [%d] : Invalid page error. It occurs due to failure in writing the correct feature configuration " "from selected page\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_SET_APS_FAIL:
+            printk(
+                "Error [%d] : APS failure error. It occurs due to failure in write of advance power mode configuration " "register\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_AUX_INVALID_CFG:
+            printk(
+                "Error [%d] : Invalid AUX configuration error. It occurs when the auxiliary interface settings are not " "enabled properly\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_AUX_BUSY:
+            printk(
+                "Error [%d] : AUX busy error. It occurs when the auxiliary interface buses are engaged while configuring" " the AUX\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_REMAP_ERROR:
+            printk(
+                "Error [%d] : Remap error. It occurs due to failure in assigning the remap axes data for all the axes " "after change in axis position\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_GYR_USER_GAIN_UPD_FAIL:
+            printk(
+                "Error [%d] : Gyro user gain update fail error. It occurs when the reading of user gain update status " "fails\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_SELF_TEST_NOT_DONE:
+            printk(
+                "Error [%d] : Self-test not done error. It occurs when the self-test process is ongoing or not " "completed\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_INVALID_INPUT:
+            printk("Error [%d] : Invalid input error. It occurs when the sensor input validity fails\r\n", rslt);
+            break;
+
+        case BMI2_E_INVALID_STATUS:
+            printk("Error [%d] : Invalid status error. It occurs when the feature/sensor validity fails\r\n", rslt);
+            break;
+
+        case BMI2_E_CRT_ERROR:
+            printk("Error [%d] : CRT error. It occurs when the CRT test has failed\r\n", rslt);
+            break;
+
+        case BMI2_E_ST_ALREADY_RUNNING:
+            printk(
+                "Error [%d] : Self-test already running error. It occurs when the self-test is already running and " "another has been initiated\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_CRT_READY_FOR_DL_FAIL_ABORT:
+            printk(
+                "Error [%d] : CRT ready for download fail abort error. It occurs when download in CRT fails due to wrong " "address location\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_DL_ERROR:
+            printk(
+                "Error [%d] : Download error. It occurs when write length exceeds that of the maximum burst length\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_PRECON_ERROR:
+            printk(
+                "Error [%d] : Pre-conditional error. It occurs when precondition to start the feature was not " "completed\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_ABORT_ERROR:
+            printk("Error [%d] : Abort error. It occurs when the device was shaken during CRT test\r\n", rslt);
+            break;
+
+        case BMI2_E_WRITE_CYCLE_ONGOING:
+            printk(
+                "Error [%d] : Write cycle ongoing error. It occurs when the write cycle is already running and another " "has been initiated\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_ST_NOT_RUNING:
+            printk(
+                "Error [%d] : Self-test is not running error. It occurs when self-test running is disabled while it's " "running\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_DATA_RDY_INT_FAILED:
+            printk(
+                "Error [%d] : Data ready interrupt error. It occurs when the sample count exceeds the FOC sample limit " "and data ready status is not updated\r\n",
+                rslt);
+            break;
+
+        case BMI2_E_INVALID_FOC_POSITION:
+            printk(
+                "Error [%d] : Invalid FOC position error. It occurs when average FOC data is obtained for the wrong" " axes\r\n",
+                rslt);
+            break;
+
+        default:
+            printk("Error [%d] : Unknown error code\r\n", rslt);
+            break;
+    }
+}
