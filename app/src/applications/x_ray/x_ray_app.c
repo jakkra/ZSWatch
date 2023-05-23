c07ccae7f20b5f49b3f6af18e8bd014bd182661e
Jakob Krantz <mail@jakobkrantz.se>
2023-05-23 22:19:55 +0200

    Add "x-ray" application. Shows the inside PCB of the watch as seen below the screen.

---
+++ app/src/applications/x_ray/x_ray_app.c
@@ -0,0 +1,42 @@
+#include <x_ray/x_ray_ui.h>
+#include <application_manager.h>
+#include <zephyr/kernel.h>
+#include <zephyr/init.h>
+#include <display_control.h>
+
+// Functions needed for all applications
+static void x_ray_app_start(lv_obj_t *root, lv_group_t *group);
+static void x_ray_app_stop(void);
+
+LV_IMG_DECLARE(circuit_icon);
+
+static application_t app = {
+    .name = "X-ray",
+    .icon = &circuit_icon,
+    .start_func = x_ray_app_start,
+    .stop_func = x_ray_app_stop
+};
+
+static uint8_t original_brightness;
+
+static void x_ray_app_start(lv_obj_t *root, lv_group_t *group)
+{
+    original_brightness = display_control_get_brightness();
+    display_control_set_brightness(100);
+    x_ray_ui_show(root);
+}
+
+static void x_ray_app_stop(void)
+{
+    display_control_set_brightness(original_brightness);
+    x_ray_ui_remove();
+}
+
+static int x_ray_app_add(const struct device *arg)
+{
+    application_manager_add_application(&app);
+
+    return 0;
+}
+
+SYS_INIT(x_ray_app_add, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
---
+++ app/src/applications/x_ray/x_ray_ui.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <inttypes.h>
+#include <lvgl.h>
+
+void x_ray_ui_show(lv_obj_t *root);
+
+void x_ray_ui_remove(void);
+
