jakkra/ZSWatch

commit 17d22ae3911e06ec00fccca4437c8c9ad4d8f166

imu: Add support to dynamically enable/disable features.
Can be used either to save power when features are not used,
or as a way to manage watch movement states.
NOTE: Right now assumptions are made that only zsw_power_manager have
control of NO_MOTION and ANY_MOTION features.

app/src/zsw_imu.c
@@ -21,6 +21,7 @@ typedef struct bmi270_feature_config_set_t {
     uint8_t                 sensor_id;
     feature_config_func     cfg_func;
     bool                    isr_disable;
+    bool                    skip_enable;
 } bmi270_feature_config_set_t;
 
 static void send_accel_event(zsw_imu_evt_t *data);
@@ -55,8 +56,8 @@ static bmi270_feature_config_set_t bmi270_enabled_features[] = {
 #if USE_ANYMOTION
     { .sensor_id = BMI2_ANY_MOTION, .cfg_func = configure_anymotion, .isr_disable = true},
 #endif
-    { .sensor_id = BMI2_STEP_ACTIVITY, .cfg_func = NULL, .isr_disable = true},
-    { .sensor_id = BMI2_WRIST_GESTURE, .cfg_func = configure_gesture_detect, .isr_disable = false},
+    { .sensor_id = BMI2_STEP_ACTIVITY, .cfg_func = NULL, .skip_enable = true},
+    { .sensor_id = BMI2_WRIST_GESTURE, .cfg_func = configure_gesture_detect},
     { .sensor_id = BMI2_WRIST_WEAR_WAKE_UP, .cfg_func = configure_wrist_wakeup},
     { .sensor_id = BMI2_NO_MOTION, .cfg_func = configure_no_motion},
 };
@@ -180,17 +181,44 @@ int zsw_imu_reset_step_count(void)
     return -ENOENT;
 }
 
-int zsw_imu_any_motion_int_set_enabled(bool enabled)
+int zsw_imu_feature_disable(zsw_imu_feature_t feature)
 {
     int8_t rslt;
+    uint16_t int_status;
+    uint8_t feature_disable;
+
+    feature_disable = feature;
+    rslt = bmi270_sensor_disable(&feature_disable, 1, &bmi2_dev);
+    bmi2_error_codes_print_result(rslt);
+
+    // Clear int_status register
+    rslt = bmi2_get_int_status(&int_status, &bmi2_dev);
+    bmi2_error_codes_print_result(rslt);
+
+    return rslt == BMI2_OK;
+}
+
+int zsw_imu_feature_enable(zsw_imu_feature_t feature, bool int_en)
+{
+    int8_t rslt;
+    uint16_t int_status;
+    uint8_t feature_disable;
     struct bmi2_sens_int_config cfg;
 
-    if (enabled) {
+    feature_disable = feature;
+    rslt = bmi270_sensor_enable(&feature_disable, 1, &bmi2_dev);
+    bmi2_error_codes_print_result(rslt);
+
+    // Clear int_status register
+    rslt = bmi2_get_int_status(&int_status, &bmi2_dev);
+    bmi2_error_codes_print_result(rslt);
+
+    if (int_en) {
         cfg.hw_int_pin = BMI2_INT2;
     } else {
         cfg.hw_int_pin = BMI2_INT_NONE;
     }
-    cfg.type = BMI2_ANY_MOTION;
+    cfg.type = feature;
 
     rslt = bmi270_map_feat_int(&cfg, 1, &bmi2_dev);
     bmi2_error_codes_print_result(rslt);
@@ -492,7 +520,9 @@ static int8_t configure_enable_all_bmi270(struct bmi2_dev *bmi2_dev)
 
     for (int i = 0; i < ARRAY_SIZE(bmi270_enabled_features); i++) {
         config[i].type = bmi270_enabled_features[i].sensor_id;
-        all_sensors[i] = bmi270_enabled_features[i].sensor_id;
+        if (!bmi270_enabled_features[i].skip_enable) {
+            all_sensors[i] = bmi270_enabled_features[i].sensor_id;
+        }
         if (is_sensor_feature(bmi270_enabled_features[i].sensor_id)) {
             all_features[num_features].type = bmi270_enabled_features[i].sensor_id;
             if (bmi270_enabled_features[i].isr_disable) {

app/src/zsw_imu.h
@@ -2,6 +2,19 @@
 #define ZSW_IMU_H_
 
 #include <zephyr/drivers/sensor.h>
+#include <bmi270.h>
+
+typedef enum zsw_imu_feature_t {
+    ZSW_IMU_FEATURE_ACCEL = BMI2_ACCEL,
+    ZSW_IMU_FEATURE_GYRO = BMI2_GYRO,
+    ZSW_IMU_FEATURE_STEP_COUNTER = BMI2_STEP_COUNTER,
+    ZSW_IMU_FEATURE_SIG_MOTION = BMI2_SIG_MOTION,
+    ZSW_IMU_FEATURE_ANY_MOTION = BMI2_ANY_MOTION,
+    ZSW_IMU_FEATURE_STEP_ACTIVITY = BMI2_STEP_ACTIVITY,
+    ZSW_IMU_FEATURE_WRIST_GESTURE = BMI2_WRIST_GESTURE,
+    ZSW_IMU_FEATURE_WRIST_WEAR_WAKE_UP = BMI2_WRIST_WEAR_WAKE_UP,
+    ZSW_IMU_FEATURE_NO_MOTION = BMI2_NO_MOTION,
+} zsw_imu_feature_t;
 
 typedef enum zsw_imu_evt_type_t {
     ZSW_IMU_EVT_TYPE_XYZ,
@@ -64,6 +77,8 @@ int zsw_imu_fetch_num_steps(uint32_t *num_steps);
 
 int zsw_imu_reset_step_count(void);
 
-int zsw_imu_any_motion_int_set_enabled(bool enabled);
+int zsw_imu_feature_enable(zsw_imu_feature_t feature, bool int_en);
+
+int zsw_imu_feature_disable(zsw_imu_feature_t feature);
 
 #endif

app/src/zsw_power_manager.c
@@ -79,11 +79,15 @@ uint32_t zsw_power_manager_get_ms_to_inactive(void)
 
 static void enter_inactive(void)
 {
-    LOG_DBG("Enter inactive");
+    LOG_INF("Enter inactive");
     is_active = false;
     retained.wakeup_time += k_uptime_get_32() - last_wakeup_time;
     retained_update();
     display_control_sleep_ctrl(false);
+
+    // Screen inactive -> wait for NO_MOTION interrupt in order to power off display regulator.
+    zsw_imu_feature_enable(ZSW_IMU_FEATURE_NO_MOTION, true);
+    zsw_imu_feature_disable(ZSW_IMU_FEATURE_ANY_MOTION);
 
     struct activity_state_event evt = {
         .state = ZSW_ACTIVITY_STATE_INACTIVE,
@@ -93,12 +97,16 @@ static void enter_inactive(void)
 
 static void enter_active(void)
 {
-    LOG_DBG("Enter active");
+    LOG_INF("Enter active");
     is_active = true;
     last_wakeup_time = k_uptime_get_32();
 
     display_control_pwr_ctrl(true);
     display_control_sleep_ctrl(true);
+
+    // Only used when display is not active.
+    zsw_imu_feature_disable(ZSW_IMU_FEATURE_NO_MOTION);
+    zsw_imu_feature_disable(ZSW_IMU_FEATURE_ANY_MOTION);
 
     struct activity_state_event evt = {
         .state = ZSW_ACTIVITY_STATE_ACTIVE,
@@ -132,17 +140,19 @@ static void zbus_accel_data_callback(const struct zbus_channel *chan)
             break;
         }
         case ZSW_IMU_EVT_TYPE_NO_MOTION: {
-            LOG_INF("Watch left stationary");
+            LOG_INF("Watch enterted stationary state");
             if (!is_active) {
                 display_control_pwr_ctrl(false);
+                zsw_imu_feature_enable(ZSW_IMU_FEATURE_ANY_MOTION, true);
+                zsw_imu_feature_disable(ZSW_IMU_FEATURE_NO_MOTION);
             }
-            zsw_imu_any_motion_int_set_enabled(true);
             break;
         }
         case ZSW_IMU_EVT_TYPE_ANY_MOTION: {
-            LOG_INF("Watch moved, prepare display");
+            LOG_INF("Watch moved, init display");
             display_control_pwr_ctrl(true);
-            zsw_imu_any_motion_int_set_enabled(false);
+            zsw_imu_feature_enable(ZSW_IMU_FEATURE_NO_MOTION, true);
+            zsw_imu_feature_disable(ZSW_IMU_FEATURE_ANY_MOTION);
             break;
         }
         default:

commit d7e4b1b2fccc8cad6a3b2246289f8c9e0ccbc95b

imu: Fix error when enabling BMI270 features. Regression from 17d22ae3911e06ec00fccca4437c8c9ad4d8f166

app/src/zsw_imu.c
@@ -517,11 +517,13 @@ static int8_t configure_enable_all_bmi270(struct bmi2_dev *bmi2_dev)
     // The API needs a list of all those features to do that map.
     struct bmi2_sens_int_config all_features[ARRAY_SIZE(bmi270_enabled_features)];
     uint8_t num_features = 0;
+    uint8_t num_enabled_features = 0;
 
     for (int i = 0; i < ARRAY_SIZE(bmi270_enabled_features); i++) {
         config[i].type = bmi270_enabled_features[i].sensor_id;
         if (!bmi270_enabled_features[i].skip_enable) {
-            all_sensors[i] = bmi270_enabled_features[i].sensor_id;
+            all_sensors[num_enabled_features] = bmi270_enabled_features[i].sensor_id;
+            num_enabled_features++;
         }
         if (is_sensor_feature(bmi270_enabled_features[i].sensor_id)) {
             all_features[num_features].type = bmi270_enabled_features[i].sensor_id;
@@ -553,7 +555,7 @@ static int8_t configure_enable_all_bmi270(struct bmi2_dev *bmi2_dev)
         /* NOTE:
         * Accel and Gyro enable must be done after setting configurations.
         */
-        rslt = bmi270_sensor_enable(all_sensors, ARRAY_SIZE(all_sensors), bmi2_dev);
+        rslt = bmi270_sensor_enable(all_sensors, num_enabled_features, bmi2_dev);
         bmi2_error_codes_print_result(rslt);
     }
 