a4caac24481e7ecfc3e5e6e609b0cb6d66ec4035
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2023-10-07 18:49:09 +0200

    - Remove MAX30101 from device tree - Make the default applications selectable via Kconfig
    Signed-off-by: Daniel Kampert <DanielKampert@kampis-elektroecke.de>

--- app/src/events/accel_event.c
+++ app/src/events/accel_event.c
@@ -5,6 +5,10 @@ ZBUS_CHAN_DEFINE(accel_data_chan,
                  struct accel_event,
                  NULL,
                  NULL,
-                 ZBUS_OBSERVERS(watchface_accel_lis, power_manager_accel_lis, zds_app_accel_lis),
+                 #ifdef CONFIG_APPLICATIONS_USE_ACCELEROMETER
+                    ZBUS_OBSERVERS(watchface_accel_lis, power_manager_accel_lis, zds_app_accel_lis),
+                 #else
+                    ZBUS_OBSERVERS(watchface_accel_lis, power_manager_accel_lis),
+                 #endif
                  ZBUS_MSG_INIT()
                 );
--- app/src/events/battery_event.c
+++ app/src/events/battery_event.c
@@ -5,6 +5,10 @@ ZBUS_CHAN_DEFINE(battery_sample_data_chan,
                  struct battery_sample_event,
                  NULL,
                  NULL,
-                 ZBUS_OBSERVERS(watchface_battery_event, zsw_phone_app_publisher_battery_event, battery_app_battery_event),
+                 #ifdef CONFIG_APPLICATIONS_USE_BATTERY
+                    ZBUS_OBSERVERS(watchface_battery_event, zsw_phone_app_publisher_battery_event, battery_app_battery_event),
+                 #else
+                    ZBUS_OBSERVERS(watchface_battery_event, zsw_phone_app_publisher_battery_event),
+                 #endif
                  ZBUS_MSG_INIT()
                 );
