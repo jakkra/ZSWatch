ad97a49676752db256f23624aa1ca401e232967b
Jakob Krantz <mail@jakobkrantz.se>
2023-10-18 08:45:30 +0200

    Add west extension 'west format' that run code formatting and new github actions to check format on PRs before they can be merged.
    Fixes https://github.com/jakkra/ZSWatch/issues/89

--- app/tools/format_code.sh
+++ app/tools/format_code.sh
@@ -1,13 +1,5 @@
 astyle \
 --options=tools/astyle.cfg \
---suffix=none \
---verbose \
---errors-to-stdout \
 --recursive src/*.c,*.h \
 --recursive drivers/*.c,*.h \
---exclude=src/images \
---exclude=src/ext_drivers \
---exclude=drivers/input \
---exclude=drivers/sensor \
---exclude=src/applications/2048/2048_lib \
 $1 $2 $3 # addtional args such as --dry-run etc.
