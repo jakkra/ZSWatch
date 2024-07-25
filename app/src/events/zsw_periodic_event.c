dab26557554586cadcd7baec4fc010b697c5756a
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2024-07-25 12:24:18 +0200

    Take version from board overlay

--- app/src/events/zsw_periodic_event.c
+++ app/src/events/zsw_periodic_event.c
@@ -1,19 +1,26 @@
-#include <zephyr/kernel.h>
 #include <zephyr/init.h>
+#include <zephyr/kernel.h>
 #include <zephyr/zbus/zbus.h>
 
+#if CONFIG_RTC
+#include <zephyr/drivers/rtc.h>
+#endif
+
+#include "zsw_clock.h"
 #include "events/periodic_event.h"
 
 #define PERIODIC_FAST_INTERVAL_MS 100
 #define PERIODIC_MID_INTERVAL_MS 1000
 #define PERIODIC_SLOW_INTERVAL_MS 10000
 
-static void handle_slow_timeout(struct k_work *item);
-static void handle_fast_timeout(struct k_work *item);
-static void handle_mid_timeout(struct k_work *item);
+#if CONFIG_RTC
+static const struct device *const rtc = DEVICE_DT_GET(DT_ALIAS(rtc));
 
-ZBUS_CHAN_DECLARE(periodic_event_10s_chan);
+void handle_mid_timeout(const struct device *dev, void *user_data);
+#endif
+
 ZBUS_CHAN_DECLARE(periodic_event_1s_chan);
+ZBUS_CHAN_DECLARE(periodic_event_10s_chan);
 ZBUS_CHAN_DECLARE(periodic_event_100ms_chan);
 
 int zsw_periodic_chan_add_obs(const struct zbus_channel *chan, const struct zbus_observer *obs)
@@ -34,7 +41,11 @@ int zsw_periodic_chan_add_obs(const struct zbus_channel *chan, const struct zbus
         if (chan == &periodic_event_10s_chan) {
             ret =  k_work_reschedule(work, K_MSEC(PERIODIC_SLOW_INTERVAL_MS));
         } else if (chan == &periodic_event_1s_chan) {
+#if CONFIG_RTC
+            rtc_update_set_callback(rtc, handle_mid_timeout, NULL);
+#else
             ret =  k_work_reschedule(work, K_MSEC(PERIODIC_MID_INTERVAL_MS));
+#endif
         } else if (chan == &periodic_event_100ms_chan) {
             ret =  k_work_reschedule(work, K_MSEC(PERIODIC_FAST_INTERVAL_MS));
         } else {
@@ -51,9 +62,16 @@ int zsw_periodic_chan_rm_obs(const struct zbus_channel *chan, const struct zbus_
     struct k_work_delayable *work = NULL;
     int ret = zbus_chan_rm_obs(chan, obs, K_MSEC(100));
     if (ret == 0 && sys_slist_is_empty(&chan->data->observers)) {
-        work = (struct k_work_delayable *)zbus_chan_user_data(chan);
-        __ASSERT(k_work_delayable_is_pending(work), "Periodic slow work is not pending");
-        ret = k_work_cancel_delayable(work);
+#if CONFIG_RTC_UPDATE
+        if (chan ==  &periodic_event_1s_chan) {
+            rtc_update_set_callback(rtc, NULL, NULL);
+        } else
+#endif
+        {
+            work = (struct k_work_delayable *)zbus_chan_user_data(chan);
+            __ASSERT(k_work_delayable_is_pending(work), "Periodic slow work is not pending");
+            ret = k_work_cancel_delayable(work);
+        }
     }
     return ret;
 }
@@ -71,6 +89,15 @@ static void handle_slow_timeout(struct k_work *item)
     zbus_chan_pub(&periodic_event_10s_chan, &evt, K_MSEC(250));
 }
 
+#if CONFIG_RTC
+void handle_mid_timeout(const struct device *dev, void *user_data)
+{
+    struct periodic_event evt = {
+    };
+
+    zbus_chan_pub(&periodic_event_1s_chan, &evt, K_MSEC(250));
+}
+#else
 static void handle_mid_timeout(struct k_work *item)
 {
     struct periodic_event evt = {
@@ -83,6 +110,7 @@ static void handle_mid_timeout(struct k_work *item)
 
     zbus_chan_pub(&periodic_event_1s_chan, &evt, K_MSEC(250));
 }
+#endif
 
 static void handle_fast_timeout(struct k_work *item)
 {
@@ -100,15 +128,22 @@ static void handle_fast_timeout(struct k_work *item)
 static int zsw_timer_init(void)
 {
     struct k_work_delayable *work = NULL;
+
     work = (struct k_work_delayable *)zbus_chan_user_data(&periodic_event_10s_chan);
     k_work_init_delayable(work, handle_slow_timeout);
 
-    // TODO: Use the RTC instead of the timer in rev 5 to generate this
+#if CONFIG_RTC
+    if (!device_is_ready(rtc)) {
+        return -EBUSY;
+    }
+#else
     work = (struct k_work_delayable *)zbus_chan_user_data(&periodic_event_1s_chan);
     k_work_init_delayable(work, handle_mid_timeout);
+#endif
 
     work = (struct k_work_delayable *)zbus_chan_user_data(&periodic_event_100ms_chan);
     k_work_init_delayable(work, handle_fast_timeout);
+
     return 0;
 }
 
