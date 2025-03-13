304385bf491314dfd18986890785bdbe75b1fe0d
Jakob Krantz <mail@jakobkrantz.se>
2025-03-13 16:38:02 +0100

    Website: Fix copyright footer.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -139,7 +139,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} ZSWatch, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} ZSWatch. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
