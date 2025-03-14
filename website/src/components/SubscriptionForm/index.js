b437f3786a42cddd01efc573130a38cff8904bd3
Jakob Krantz <mail@jakobkrantz.se>
2025-03-14 17:15:24 +0100

    Website: Move Subrsciption form into a seperate component.
    Allows us to reause it wherever needed.

---
+++ website/src/components/SubscriptionForm/index.js
@@ -0,0 +1,22 @@
+
+import styles from './styles.module.css';
+
+export default function SubscriptionForm() {
+  return (
+    <form method="post" action="https://zswatch.pikapod.net/subscription/form" className={styles.signup}>
+      <div>
+        <div className={styles.signupTitle}>Sign up for updates here</div>
+        <input type="hidden" name="nonce" />
+        <div>
+          <input type="email" name="email" required placeholder="Enter your email" className={styles.emailInput} />
+          <input type="submit" value="Subscribe" className={styles.subscribeBtn} />
+        </div>
+        <p hidden>
+          <input id="2cca0" type="checkbox" name="l" defaultChecked value="2cca0c50-ef0d-4339-b695-9e559f4a4359" />
+          <label htmlFor="2cca0">ZSWatch Test List</label>
+          <br />
+        </p>
+      </div>
+    </form>
+  );
+}
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -3,6 +3,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import SubscriptionForm from '@site/src/components/SubscriptionForm';
 
 import styles from './index.module.css';
 
@@ -19,26 +20,6 @@ function HomepageHeader() {
         </div>
       </div>
     </header>
-  );
-}
-
-function SubscriptionForm() {
-  return (
-    <form method="post" action="https://zswatch.pikapod.net/subscription/form" className={styles.signup}>
-      <div>
-        <div className={styles.signupTitle}>Sign up for updates here</div>
-        <input type="hidden" name="nonce" />
-        <div>
-          <input type="email" name="email" required placeholder="Enter your email" className={styles.emailInput} />
-          <input type="submit" value="Subscribe" className={styles.subscribeBtn} />
-        </div>
-        <p hidden>
-          <input id="2cca0" type="checkbox" name="l" defaultChecked value="2cca0c50-ef0d-4339-b695-9e559f4a4359" />
-          <label htmlFor="2cca0">ZSWatch Test List</label>
-          <br />
-        </p>
-      </div>
-    </form>
   );
 }
 
