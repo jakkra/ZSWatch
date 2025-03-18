67967db577f455e780e84a2a0f8751913b051e2e
Jakob Krantz <mail@jakobkrantz.se>
2025-03-18 19:03:20 +0100

    Website: Prepare for deploying on zswatch.dev

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://zswatch.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ZSWatch/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
