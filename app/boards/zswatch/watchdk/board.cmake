914091425ce8971661a34cc968ca2202aed829d3
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2025-06-29 20:26:05 +0200

    Add board definition for Development Kit

---
+++ app/boards/zswatch/watchdk/board.cmake
@@ -0,0 +1,22 @@
+# Copyright (c) 2025 ZSWatch Project
+# SPDX-License-Identifier: Apache-2.0
+
+if(CONFIG_BOARD_WATCHDK_NRF5340_CPUAPP_NS)
+  set(TFM_PUBLIC_KEY_FORMAT "full")
+endif()
+
+if(CONFIG_BOARD_WATCHDK_NRF5340_CPUAPP OR CONFIG_BOARD_WATCHDK_NRF5340_CPUAPP_NS)
+  board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
+endif()
+
+if(CONFIG_TFM_FLASH_MERGED_BINARY)
+  set_property(TARGET runners_yaml_props_target PROPERTY hex_file tfm_merged.hex)
+endif()
+
+if(CONFIG_BOARD_WATCHDK_NRF5340_CPUNET)
+  board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
+endif()
+
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
