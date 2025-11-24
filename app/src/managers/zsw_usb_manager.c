3d70eb80d5eb39948766a1fd1c733200d3cab7dc
Jakob Krantz <mail@jakobkrantz.se>
2025-11-24 23:37:42 +0100

    Fix crash when XIP was disabled before USB.
    Reason is the usb_disable is async. hence we can't disable XIP until later.
    There was no suitable callback found, so just use a delay.

--- app/src/managers/zsw_usb_manager.c
+++ app/src/managers/zsw_usb_manager.c
@@ -22,9 +22,11 @@ LOG_MODULE_REGISTER(zsw_usb_manager, LOG_LEVEL_INF);
 static const struct device *const cdc_acm_dev = DEVICE_DT_GET(ZSW_USB_ACM_NODE);
 
 static void boot_timeout_handler(struct k_work *work);
+static void xip_release_work_handler(struct k_work *work);
 
 static K_MUTEX_DEFINE(state_lock);
 static K_WORK_DELAYABLE_DEFINE(boot_timeout_work, boot_timeout_handler);
+static K_WORK_DELAYABLE_DEFINE(xip_release_work, xip_release_work_handler);
 
 static int usb_refcount;
 
@@ -43,6 +45,17 @@ static void boot_timeout_handler(struct k_work *work)
     }
 }
 
+static void xip_release_work_handler(struct k_work *work)
+{
+    ARG_UNUSED(work);
+
+    if (zsw_usb_manager_is_enabled()) {
+        return;
+    }
+
+    zsw_xip_disable();
+}
+
 int _zsw_usb_manager_enable(const char *requester)
 {
     int ret = 0;
@@ -52,6 +65,7 @@ int _zsw_usb_manager_enable(const char *requester)
     LOG_INF("USB power on (%s => %d)", requester, usb_refcount);
 
     if (usb_refcount == 1) {
+        k_work_cancel_delayable(&xip_release_work);
         zsw_xip_enable();
         ret = usb_enable(NULL);
         __ASSERT(ret == 0, "usb_enable failed (%d)", ret);
@@ -80,8 +94,12 @@ int _zsw_usb_manager_disable(const char *requester)
     if (usb_refcount == 0) {
         ret = usb_disable();
         __ASSERT(ret == 0, "usb_disable failed (%d)", ret);
-        zsw_xip_disable();
         LOG_INF("Disable USB stack");
+        // The usb_disable() call above takes some time to fully complete, and
+        // finishes in another context. So we need to wait before disabling XIP.
+        // Seems as the USB user callback can't be used as it's not getting called when disabled.
+        // So we use a delayed work instead.
+        k_work_reschedule(&xip_release_work, K_MSEC(50));
     }
 
     k_mutex_unlock(&state_lock);
