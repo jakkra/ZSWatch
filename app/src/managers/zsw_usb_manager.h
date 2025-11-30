5e61879e04f59f7b7e6bb2163b5bd8e066b75a23
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2025-11-30 12:06:10 +0100

    Add license text

--- app/src/managers/zsw_usb_manager.h
+++ app/src/managers/zsw_usb_manager.h
@@ -1,5 +1,21 @@
-#ifndef ZSW_USB_MANAGER_H
-#define ZSW_USB_MANAGER_H
+/*
+ * This file is part of ZSWatch project <https://github.com/zswatch/>.
+ * Copyright (c) 2025 ZSWatch Project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
 
 #include <stdbool.h>
 #include <errno.h>
@@ -15,5 +31,3 @@ bool zsw_usb_manager_is_enabled(void);
 #define zsw_usb_manager_disable() (0)
 #define zsw_usb_manager_is_enabled() (false)
 #endif
-
-#endif // ZSW_USB_MANAGER_H
