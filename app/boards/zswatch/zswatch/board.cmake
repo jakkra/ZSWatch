8670c1ffc4b10fe7c3d03bc9b52219a2fc812903
Jakob Krantz <mail@jakobkrantz.se>
2026-04-10 21:37:02 +0200

    Add new zswatch board, small watch sized board based on watchdk.

---
+++ app/boards/zswatch/zswatch/board.cmake
@@ -0,0 +1,23 @@
+# Copyright (c) 2025 ZSWatch Project
+# SPDX-License-Identifier: Apache-2.0
+
+if(CONFIG_BOARD_ZSWATCH_NRF5340_CPUAPP_NS)
+  set(TFM_PUBLIC_KEY_FORMAT "full")
+endif()
+
+if(CONFIG_BOARD_ZSWATCH_NRF5340_CPUAPP OR CONFIG_BOARD_ZSWATCH_NRF5340_CPUAPP_NS)
+board_runner_args(nrfjprog "--qspiini=${BOARD_DIR}/support/qspi_mx25u51245.ini")
+  board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
+endif()
+
+if(CONFIG_TFM_FLASH_MERGED_BINARY)
+  set_property(TARGET runners_yaml_props_target PROPERTY hex_file tfm_merged.hex)
+endif()
+
+if(CONFIG_BOARD_ZSWATCH_NRF5340_CPUNET)
+  board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
+endif()
+
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
